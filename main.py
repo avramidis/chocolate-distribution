c2ba4da38c7a978f2502cae9874a701299687b36
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-07-11 20:49:17 +0100

    Removed deprecated commented code.

--- main.py
+++ main.py
@@ -23,69 +23,34 @@ def giveChocolate(chocolates, children):
         print("Error in childer input - negative values!")
         return -1
 
-    # print(chocolates)
-    # print(children)
-
-    cuts = 0
-
-    # childpot = [[] for _ in range(len(children))]
-    # print(childpot)
-
-    #print(sum(chocolates))
-    #print(sum(children))
-
     children.sort(reverse=True)
-    # print(children)
     chocolates.sort(reverse=True)
 
     # for each child find the exact chocolate size they want
-    #fullChildren = []
     for c in range(len(children)):
         for p in range(len(chocolates)):
             diff = getDiff(children[c], chocolates[p])
             if diff==0:
-                # childpot[c].append(chocolates[p])
                 chocolates.pop(p)
-                #fullChildren.append(c)
                 children[c]=0
                 break
 
-    
-    # print(children)  
     children = list(filter(lambda a: a != 0, children))
-    # print("Remove zeros")    
-    # print(children)    
-            #s = str(children[c]) + ' ' +  str(chocolates[p]) + ' ' + str(diff)
-            #print(s)
 
-    # print(childpot)
-    # print(fullChildren)
-
-    #for c in range(len(children)):
+    cuts = 0
     c = 0
     while c < len(children):
-        # if c in fullChildren:
-        #     c=c+1
-        #     continue
-        
-        # print(str(c))
 
         for p in range(len(chocolates)):
             diff = getDiff(children[c], chocolates[p])
-            s = str(children[c]) + ' ' +  str(chocolates[p]) + ' ' + str(diff) + ' ' + str(cuts)
-            #s = str(c) + ' ' +  str(chocolates[p]) + ' ' + str(diff)
-            # print(s)
 
             if diff < 0:
                 children[c] = children[c] - chocolates[p]
                 chocolates.pop(p)
                 chocolates.append(abs(diff))
-                ##cuts = cuts + 1
                 children.sort(reverse=True)
                 break
             else:
-                
-                #children[c]=0
                 children.pop(c)
                 
                 chocolates.pop(p)
@@ -94,13 +59,7 @@ def giveChocolate(chocolates, children):
                 if diff>0:
                     cuts = cuts + 1
 
-                #c = c + 1
                 break
-
-    # print('Number of cuts: ' + str(cuts))
-    # print('Chocolate left ' + str(sum(chocolates)))
-    # print('Children left ' + str(sum(children)))
-    # print(chocolates)
 
     return cuts
 
