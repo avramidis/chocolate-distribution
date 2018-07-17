196ebfd017bf38302c94705f9cda5b136595956b
Eleftherios Avramidis <ea461@cam.ac.uk>
2018-07-17 12:37:23 +0100

    Added script with basic setup functionality.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='chocolatedistribution',
+      version='1.0',
+      description='Python package that calculates the cuts for distributing chocolate to children.',
+      packages=['chocolatedistribution'],
+      license='MIT'
+     )
